0c70768ddeae90c7f0ee6571714bde222e6c26ca
edward mccaughan <emc@betterplace.org>
2013-07-10 16:51:57 +0200

    added basic "how it works" page

--- server.rb
+++ server.rb
@@ -15,10 +15,10 @@ end
 get '/donate' do
   order_id = Compliments.add_compliment_order(params[:twitter_handle], params[:compliment])
 
-  betterplace_donation_url = "http://www.betterplace.dev/projects/1114/client_donations/new"  # go to the the betterplace.org donation form
-  betterplace_donation_url << "?client_id=bettercompliments"                                  # betterplace.org will use this to figure out your callback url
-  betterplace_donation_url << "&donation_presenter[donation_client_reference]=#{order_id}"    # betterplace will return this to the callback so you can figure out what the donation was for
-  betterplace_donation_url << "&donation_presenter[donation_amount]=6"                        # set the default amount for the user to donate
+  betterplace_donation_url = "http://www.bp42.com/en/projects/1114/client_donations/new"  # go to the the betterplace.org donation form
+  betterplace_donation_url << "?client_id=bettercompliments"                                     # betterplace.org will use this to figure out your callback url
+  betterplace_donation_url << "&donation_presenter[donation_client_reference]=#{order_id}"       # betterplace will return this to the callback so you can figure out what the donation was for
+  betterplace_donation_url << "&donation_presenter[donation_amount]=6"                           # set the default amount for the user to donate
 
   redirect to(betterplace_donation_url)
 end
@@ -34,4 +34,8 @@ get '/postdonate' do
   else
     raise "nnnnnnnnnnnnnoooooooooo, the donation was unsucessful"
   end
+end
+
+get '/how_it_works' do
+  erb :how_it_works
 end
